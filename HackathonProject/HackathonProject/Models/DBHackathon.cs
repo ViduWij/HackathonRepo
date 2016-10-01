064dc3f82a0ddd16e652f63553c6967ce4854092
ViduWij <prabhavi313@gmail.com>
2016-10-01 21:38:14 +0530

    Added Database and intial Migration

---
+++ HackathonProject/HackathonProject/Models/DBHackathon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HackathonProject.Models
+{
+    public class DBHackathon :DbContext
+    {
+        public DBHackathon() : base("DBHackathon")
+        {
+            
+        }
+    }
+}
